9d65f232220b6f654c8704d64f20d1e227bf3f45
Viktor Rengel <viktorengel@hotmail.com>
2024-01-03 21:53:30 -0500

    Middleware, Layouts con navegación, formularios

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,25 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/','welcome')->name('welcome');
-
-Route::get('/chirps', function(){
-    return 'Welcome to our chirps page';
-})->name('chirps.index');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('/','welcome')->name('welcome');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    
+    Route::get('/chirps', function(){
+        return view('chirps.index');
+    })->name('chirps.index');
+
+    Route::post('/chirps', function(){
+        //return 'Processing Chirps...';
+        //return request('message');
+        $message =  request('message');
+        // Insert into data base
+    });
 });
 
 require __DIR__.'/auth.php';
